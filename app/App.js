227d2e30fa0b33c205f0a24ec5cfb888c00e7212
王红宾 <wanghongbin@wanghongbindeMacBook-Pro.local>
2018-06-04 21:09:00 +0800

    organize entry file

--- app/App.js
+++ app/App.js
@@ -4,55 +4,32 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React from "react";
+import Ionicons from "@expo/vector-icons/fonts/Ionicons.ttf";
+import App from "./src/App";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const { AppLoading, Font } = Expo;
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class ExpoApp extends App {
+  state = {
+    isReady: false
+  };
+
+  componentWillMount() {
+    this.loadFonts();
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+      Ionicons
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+    return super.render();
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
